7fbdeaef847294bb66c3732c25f9cbcb2ed288cf
Rae Hebron <rae.oxford.hebron@gmail.com>
2022-11-25 10:42:18 -0500

    create and index route

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ require("dotenv").config()
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT
+let scientists = []
 
 
 // Middleware
 app.use(express.urlencoded({extended: true}))
+
+// Index Route
+app.get("/planet", (req, res) => {
+    res.render('index.ejs', {
+        scientists
+    })
+})
 
 // New Route
 app.get("/planet/new", (req, res) => {
@@ -13,6 +21,11 @@ app.get("/planet/new", (req, res) => {
 })
 
 // Create Route
+app.post("/planet", (req, res) => {
+    scientists.unshift(req.body)
+    console.log(scientists)
+    res.redirect("/planet")
+})
 
 app.listen(PORT, () => {
     console.log(`server is listening to port ${PORT}`)
